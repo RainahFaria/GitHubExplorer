148d7d8438a504ce3eb9e75fa34f8cd5aa4fd5a6
Rainah Faria da Silva <rainahfaria2@gmail.com>
2020-08-16 17:01:31 -0300

    save repositories and add details page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,57 +1,96 @@
-import React from 'react';
+import React, { FormEvent, useEffect, useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
 import logo from '../../assets/logo.svg';
-import { Form, Repositories, Title } from './styles';
+import api from '../services/api';
+import { Error, Form, Repositories, Title } from './styles';
 
-
+interface Repository {
+  full_name: string;
+  description: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
 
 const Dashboard: React.FC = () => {
-    return (
-        <>
-            <img src={logo} alt="GitHub Explorer" />
-            <Title>Explore repositórios no GitHub</Title>
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      '@GithubExplorer:repositories'
+    );
 
-            <Form>
-                <input type="text" placeholder="Digite o nome do repositório"/>
-                <button type="submit">Pesquisar</button>
-            </Form>
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+    return [];
 
-            <Repositories>
-                <a href="teste">
-                    <img src="https://avatars1.githubusercontent.com/u/63857037?s=460&u=f81f9e46ba69892735e44a72bf90caeca6e10a75&v=4" 
-                    alt="Rainah Faria"/>
-                    <div>
-                        <strong>RainahFaria/Ecoleta</strong>
-                        <p>Cadastro e pesquisa de pontos de coleta de resíduos.</p>
-                    </div>
+  });
+  const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
 
-                    <FiChevronRight size={20}/>
-                </a>
+  useEffect(() => {
+    localStorage.setItem(
+      '@GithubExplorer:repositories',
+      JSON.stringify(repositories)
+    );
+  }, [repositories]);
 
-                <a href="teste">
-                    <img src="https://avatars1.githubusercontent.com/u/63857037?s=460&u=f81f9e46ba69892735e44a72bf90caeca6e10a75&v=4" 
-                    alt="Rainah Faria"/>
-                    <div>
-                        <strong>RainahFaria/Ecoleta</strong>
-                        <p>Cadastro e pesquisa de pontos de coleta de resíduos.</p>
-                    </div>
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
 
-                    <FiChevronRight size={20}/>
-                </a>
+    if (!newRepo) {
+      setInputError('Digite o autor/nome do repositório');
+      return;
+    }
 
-                <a href="teste">
-                    <img src="https://avatars1.githubusercontent.com/u/63857037?s=460&u=f81f9e46ba69892735e44a72bf90caeca6e10a75&v=4" 
-                    alt="Rainah Faria"/>
-                    <div>
-                        <strong>RainahFaria/Ecoleta</strong>
-                        <p>Cadastro e pesquisa de pontos de coleta de resíduos.</p>
-                    </div>
+    try {
+      const response = await api.get<Repository>(`repos/${newRepo}`);
+      const repository = response.data;
 
-                    <FiChevronRight size={20}/>
-                </a>
-            </Repositories>
-        </>
-    )
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (error) {
+      setInputError('Erro na busca desse repositório');
+    }
+
+  }
+  return (
+    <>
+      <img src={logo} alt="GitHub Explorer" />
+      <Title>Explore repositórios no GitHub</Title>
+
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          onChange={(e) => setNewRepo(e.target.value)}
+          type="text"
+          placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      {inputError && <Error>{inputError}</Error>}
+
+      <Repositories>
+        {repositories.map((repository) => (
+          <Link
+            key={repository.full_name}
+            to={`/repositories/${repository.full_name}`}
+          >
+            <img src={repository.owner.avatar_url}
+              alt={repository.owner.login} />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </Link>
+        ))}
+      </Repositories>
+    </>
+  )
 }
 
 export default Dashboard;
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from 'styled-components';
-import {shade} from 'polished';
+import styled, { css } from 'styled-components';
+import { shade } from 'polished';
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
     font-size: 48px;
@@ -10,7 +14,7 @@ export const Title = styled.h1`
     line-height: 56px;
 `
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
     margin-top: 40px;
     max-width: 700px;
     display: flex;
@@ -20,8 +24,15 @@ export const Form = styled.form`
         height:70px;
         padding: 0 24px;
         border: 0;
-        border-radius: 5px 0 0 5px;        
+        border-radius: 5px 0 0 5px;
         color: #3a3a3a;
+        border: 2px solid #fff;
+        border-right: 0;
+
+        ${(props) =>
+    props.hasError && css`
+          border-color: #c53030;
+          `}
 
         &::placeholder {
             color: #a8a8b3;
@@ -29,7 +40,7 @@ export const Form = styled.form`
     }
 
     button {
-        width:210px;        
+        width:210px;
         height:70px;
         background: #04d361;
         border-radius: 0px 5px 5px 5px 0px;
@@ -42,6 +53,11 @@ export const Form = styled.form`
             background: ${shade(0.2, '#04d361')}
         }
     }
+`
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
+  margin-top:8px;
 `
 
 export const Repositories = styled.div`
@@ -75,10 +91,11 @@ export const Repositories = styled.div`
         }
 
         div {
-            margin-left:16px;
+            margin: 0 16px;
+            flex: 1;
 
             strong {
-                font-size: 20px;    
+                font-size: 20px;
                 color: #3a3a3a;
             }
 
@@ -88,7 +105,7 @@ export const Repositories = styled.div`
                 margin-top: 4px;
             }
 
-        }        
+        }
 
         svg {
             margin-left: auto;
